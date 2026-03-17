af91da3f5627673f8993b0fb59028ec0d71e339b
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-17 14:57:26 -0300

    exercicio 4: concluido
    code :dividir dois numeros e validar se divisão foi porzero

--- code/index.js
+++ code/index.js
@@ -1,10 +1,14 @@
 console.log( "Lista de 100 exercicios de logica com JS")
 
-// ====== EXERCICIO 3  ===============
+// ====== EXERCICIO 4  ===============
 
-//3. Multiplicar dois números — Domínio: operadores matemáticos
+//4. Divisão com validação de zero — Domínio: if/else.
 
-let num1 = 10 ; 
-let num2 = 10 ;  
-let mult = num1 * num2; 
-console.log(`O resultado de ${num1} X ${num2} = ${mult}`)
+let num1 =  100 ; 
+let num2 =  20 ;  
+let div= num1 / num2;
+if (num1 === 0  || num2 === 0  ){ 
+    console.log(`Essa divisão e por zero`)
+} else { 
+    console.log(`Essa divisão e valida, resultado : ${div} `)
+}
