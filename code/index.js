0608e961a63206c13a60e94fd97a09cdca8b581b
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-17 10:06:37 -0300

    Exercicio 1 - Concluido
    code: soma de dois numeros

---
+++ code/index.js
@@ -0,0 +1,10 @@
+console.log( "Lista de 100 exercicios de logica com JS")
+
+// ====== EXERCICIO 1  ===============
+
+//1. Soma de dois números — Domínio: variáveis e operadores aritméticos.
+
+let num1 = 5; 
+let num2 = 10; 
+let soma = num1 + num2; 
+console.log(`A soma dos numeros e  : ${soma}`); 
