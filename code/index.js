467b4834956087683464ae32a013c0fdd2792b18
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-17 13:07:19 -0300

    exercicio 2 - concluido
    code: subtrair dois numeros

--- code/index.js
+++ code/index.js
@@ -1,10 +1,10 @@
 console.log( "Lista de 100 exercicios de logica com JS")
 
-// ====== EXERCICIO 1  ===============
+// ====== EXERCICIO 2  ===============
 
-//1. Soma de dois números — Domínio: variáveis e operadores aritméticos.
+//2. Subtração de dois números — Domínio: operadores matemáticos
 
-let num1 = 5; 
-let num2 = 10; 
-let soma = num1 + num2; 
-console.log(`A soma dos numeros e  : ${soma}`); 
+let num1 = 60 ; 
+let num2 = 30 ;  
+let sub = num1 - num2; 
+console.log(`O resultado de ${num1} - ${num2} = ${sub}`)
