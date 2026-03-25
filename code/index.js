bec1d780090813b5f96b270cf4b87e34dcc88762
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-25 11:50:22 -0300

    exercicio 8 : concluido
    code: calcular a media de tres nota

--- code/index.js
+++ code/index.js
@@ -1,15 +1,28 @@
 console.log( "Lista de 100 exercicios de logica com JS")
 
-// ====== EXERCICIO 7 ===============
+// ====== EXERCICIO 8 ===============
 
-//7.  Positivo, negativo ou zero — Domínio: condicionais.
+//8. Média de três notas — Domínio: operadores + condição
 
-let num = -1  
+let nota1 =  9
+let nota2 = 9
+let nota3 =  9
 
-if (num > 0) { 
-    console.log(`${num} e POSITIVO`)
-} else if (num< 0) { 
-    console.log(`${num} e NEGATIVO`)  
-} else { 
-    console.log(`${num} e igual a ZERO`)  
-}; 
+const calculoDaMedia = (nota1 + nota2 + nota3)/3 
+const notaFinal = Math.round(calculoDaMedia) 
+//console.log( notaFinal)
+
+switch(true){ 
+    case (notaFinal <6) : 
+        console.log(`vc tirou :${notaFinal} pontos, esta abaixo da media`)
+    break
+    case (notaFinal === 6) : 
+        console.log(`vc tirou :${notaFinal} pontos, esta na media`)
+    break 
+    case (notaFinal > 6 && notaFinal<=9 )  : 
+        console.log(`vc tirou :${notaFinal} pontos, esta acima da media`)
+    break 
+    case (notaFinal === 10) : 
+        console.log(`vc tirou :${notaFinal} pontos, sua nota e maxima, Parabens !`)
+}
+
