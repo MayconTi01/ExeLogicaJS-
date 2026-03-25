b8e22dfcc10d5e436cca70e17422057edfbf4efd
MayconRodrigues <maycon.rodrigues@novoseguros.com.br>
2026-03-25 11:13:14 -0300

    exercicio 7 : Concluido
    code: verificar ser numero e positivo, negativo ou zero

--- code/index.js
+++ code/index.js
@@ -1,16 +1,15 @@
 console.log( "Lista de 100 exercicios de logica com JS")
 
-// ====== EXERCICIO 6 ===============
+// ====== EXERCICIO 7 ===============
 
-//6. Maior entre dois números — Domínio: comparação.
+//7.  Positivo, negativo ou zero — Domínio: condicionais.
 
-let num1 = 10 
-let num2 = 10
+let num = -1  
 
-if (num1 > num2 ){ 
-    console.log(`${num1} e maior que ${num2}`)
-} else if ( num1 < num2 ) { 
-    console.log(`${num2} e maior que ${num1}`)
+if (num > 0) { 
+    console.log(`${num} e POSITIVO`)
+} else if (num< 0) { 
+    console.log(`${num} e NEGATIVO`)  
 } else { 
-    console.log( ' os numero são iguais')
-}
+    console.log(`${num} e igual a ZERO`)  
+}; 
